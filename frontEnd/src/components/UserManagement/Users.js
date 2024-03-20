abcfd2840adaed33b8c9af8146ad53054f3a5737
thathsaraDinu <it22306968@my.sliit.lk>
2024-03-20 10:40:27 +0530

    Revert "Revert "UI update""
    This reverts commit c17a468b722a4af2c91d267a6b250ac42c88da7f.

--- frontEnd/src/components/UserManagement/Users.js
+++ frontEnd/src/components/UserManagement/Users.js
@@ -51,10 +51,23 @@ const Users = () => {
           flexDirection: "column",
           justifyContent: "center",
           alignItems: "center",
-
+          paddingLeft: "30px",
+          paddingRight: "30px",
           transition: "margin-left 0.5s",
         }}
       >
+        <img
+          style={{
+            position: "absolute",
+            left: "0",
+            top: "0",
+            width: "100%",
+            zIndex: "0",
+          }}
+          src="/pexels-pavel-danilyuk-6407556.jpg"
+          class="brightness-50 object-cover h-full"
+          alt=""
+        ></img>
         <br></br>
         <h1
           className="text-3xl"
@@ -63,8 +76,8 @@ const Users = () => {
           Customers{" "}
         </h1>
         <table
-          class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400"
-          style={{ width: "1200px", marginTop: "50px" }}
+          class="z-10 overflow-x-auto mx-5 w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400"
+          style={{ marginTop: "50px" }}
         >
           <thead class="text-xs  uppercase  bg-gray-700 text-gray-200">
             <tr>
@@ -80,15 +93,11 @@ const Users = () => {
               <th scope="col" class="px-6 py-3">
                 Address
               </th>
+
               <th scope="col" class="px-6 py-3">
-                Billing Address
+                UserType
               </th>
-              <th scope="col" class="px-6 py-3">
-                City
-              </th>
-              <th scope="col" class="px-6 py-3">
-                Communication Prefference
-              </th>
+
               <th scope="col" class="px-6 py-3">
                 Actions
               </th>
@@ -104,9 +113,8 @@ const Users = () => {
                 <td class="px-6 py-4">{users.phoneNumber}</td>
                 <td class="px-6 py-4">{users.email}</td>
                 <td class="px-6 py-4">{users.address}</td>
-                <td class="px-6 py-4">{users.NIC}</td>
-                <td class="px-6 py-4">{users.city}</td>
-                <td class="px-6 py-4">{users.comPreff}</td>
+
+                <td class="px-6 py-4">{users.usertype}</td>
 
                 <td
                   style={{
@@ -145,7 +153,7 @@ const Users = () => {
           color="success"
           type="button"
           to={"/TheForm"}
-          class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+          class="z-20 focus:outline-none text-white bg-green-800 hover:bg-green-900 focus:ring-4 focus:ring-green-300 text-base rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-700 dark:hover:bg-green-800 dark:focus:ring-green-900"
         >
           Add
         </Link>
