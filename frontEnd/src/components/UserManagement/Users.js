e203d6b33aa9c81948efa5a1db511950a7befa34
thathsaraDinu <it22306968@my.sliit.lk>
2024-04-08 14:43:15 +0530

    search option update

--- frontEnd/src/components/UserManagement/Users.js
+++ frontEnd/src/components/UserManagement/Users.js
@@ -90,7 +90,7 @@ const Users = () => {
                 </h1>
 
                 <div className="flex justify-end">
-                  <form className="w-80 float-right">
+                  <div className="w-80 float-right">
                     <label
                       htmlFor="default-search"
                       className="mb-2 text-sm font-medium text-gray-900 sr-only"
@@ -123,14 +123,9 @@ const Users = () => {
                         onChange={(e) => setSearch(e.target.value)}
                         required
                       />
-                      <button
-                        type="submit"
-                        className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2"
-                      >
-                        Search
-                      </button>
+                      
                     </div>
-                  </form>
+                  </div>
                 </div>
                 <div
                   style={{
