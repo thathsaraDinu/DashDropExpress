bb9ee0479b3bd8a741e9adc202e64f9bd03f4807
pasindu <IT22355928@my.sliit.lk>
2024-05-06 23:08:32 +0530

    Update DriverTable.js

--- frontEnd/src/components/DeliveryManagement/DriverTable.js
+++ frontEnd/src/components/DeliveryManagement/DriverTable.js
@@ -59,7 +59,7 @@ const DriverTable = ({ rows }) => {
         <TableContainer
           sx={{ backgroundColor: "rgba(192,192,192,0.6)" }}
           component={Paper}
-
+>
       
           <Table
             sx={{
