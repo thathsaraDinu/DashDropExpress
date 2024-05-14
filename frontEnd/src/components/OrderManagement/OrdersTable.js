e308f215486b1720cd47b947c01ed8ee66a760a8
pasindu <IT22355928@my.sliit.lk>
2024-05-14 16:01:21 +0530

    added the pdf

--- frontEnd/src/components/OrderManagement/OrdersTable.js
+++ frontEnd/src/components/OrderManagement/OrdersTable.js
@@ -1,92 +1,201 @@
 import { useState } from "react";
-import { Button,  Paper,   Table, TableBody, TableCell, TableContainer, TableHead, TableRow,TextField } from "@mui/material";
+import {
+  Button,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TextField,
+} from "@mui/material";
+import jsPDF from "jspdf";
+import "jspdf-autotable";
 
+const OrdersTable = ({ rows, selectedOrder, deleteOrder }) => {
+  const [searchTerm, setSearchTerm] = useState("");
 
-const OrdersTable = ({rows , selectedOrder , deleteOrder}) =>{
-    const [searchTerm, setSearchTerm] = useState("");
+  const handleDownloadPdf = () => {
+    const doc = new jsPDF();
+    doc.autoTable({
+      head: [
+        [
+          "Order ID",
+          "Order Date",
+          "Sender Name",
+          "Sender Address",
+          "Sender Contact No",
+          "Sender Email",
+          "Recipient Name",
+          "Recipient Address",
+          "Recipient Contact No",
+          "Item Type",
+          "Quantity",
+          "Weight",
+        ],
+      ],
+      body: rows.map((row) => [
+        row.id,
+        row.date,
+        row.name,
+        row.address,
+        row.contact,
+        row.email,
+        row.Name,
+        row.Address,
+        row.Contact,
+        row.item,
+        row.quantity,
+        row.weight,
+      ]),
+    });
+    doc.save("Order_Table.pdf");
+  };
 
-    const handleSearchChange = (e) => {
-        setSearchTerm(e.target.value);
-    };
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
 
-    const filteredRows = rows.filter((row) =>
-        Object.values(row).some(
-            (value) => typeof value === "string" && value.toLowerCase().includes(searchTerm.toLowerCase())
-        )
-    );
-    return(
-        <div className="mb-20">
-            <TextField
-                variant="outlined"
-                label="Search"
-                value={searchTerm}
-                onChange={handleSearchChange}
-                sx={{ marginBottom: "10px" }}
-            />
-    <TableContainer component={Paper} >
+  const filteredRows = rows.filter((row) =>
+    Object.values(row).some(
+      (value) =>
+        typeof value === "string" &&
+        value.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+  );
+  return (
+    <div className="mb-20">
+      <TextField
+        variant="outlined"
+        label="Search"
+        value={searchTerm}
+        onChange={handleSearchChange}
+        sx={{ marginBottom: "10px" }}
+      />
+      <TableContainer component={Paper}>
         <Table>
-            <TableHead>
-                <TableRow sx={{ backgroundColor: '#A1E8AB' }}>
-                    <TableCell> Order ID</TableCell>
-                    <TableCell> Order Date</TableCell>
-                    <TableCell>Sender Name</TableCell>
-                    <TableCell>Sender Address</TableCell>
-                    <TableCell>Sender Contact No</TableCell>
-                    <TableCell>Sender Email</TableCell>
-                    <TableCell>Recipient Name</TableCell>
-                    <TableCell>Recipient Address</TableCell>
-                    <TableCell>Recipient Contact No</TableCell>
-                    <TableCell>Item Type</TableCell>
-                    <TableCell>Quantity</TableCell>
-                    <TableCell> Weight (in Kg)</TableCell>
-                    <TableCell>Actions</TableCell>
+          <TableHead>
+            <TableRow sx={{ backgroundColor: "#A1E8AB" }}>
+              <TableCell> Order ID</TableCell>
+              <TableCell> Order Date</TableCell>
+              <TableCell>Sender Name</TableCell>
+              <TableCell>Sender Address</TableCell>
+              <TableCell>Sender Contact No</TableCell>
+              <TableCell>Sender Email</TableCell>
+              <TableCell>Recipient Name</TableCell>
+              <TableCell>Recipient Address</TableCell>
+              <TableCell>Recipient Contact No</TableCell>
+              <TableCell>Item Type</TableCell>
+              <TableCell>Quantity</TableCell>
+              <TableCell> Weight (in Kg)</TableCell>
+              <TableCell>Actions</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {filteredRows.length > 0 ? (
+              filteredRows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  sx={{
+                    "&:last-child td,&:last-child th": { border: 0 },
+                    backgroundColor: "#BEE8D6",
+                  }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.id}
+                  </TableCell>
+                  <TableCell component="th" scope="row">
+                    {row.date}
+                  </TableCell>
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell component="th" scope="row">
+                    {row.address}
+                  </TableCell>
+                  <TableCell component="th" scope="row">
+                    {row.contact}
+                  </TableCell>
+                  <TableCell component="th" scope="row">
+                    {row.email}
+                  </TableCell>
+                  <TableCell component="th" scope="row">
+                    {row.Name}
+                  </TableCell>
+                  <TableCell component="th" scope="row">
+                    {row.Address}
+                  </TableCell>
+                  <TableCell component="th" scope="row">
+                    {row.Contact}
+                  </TableCell>
+                  <TableCell component="th" scope="row">
+                    {row.item}
+                  </TableCell>
+                  <TableCell component="th" scope="row">
+                    {row.quantity}
+                  </TableCell>
+                  <TableCell component="th" scope="row">
+                    {row.weight}
+                  </TableCell>
+
+                  <TableCell>
+                    <Button
+                      sx={{
+                        margin: "0px 10px",
+                        backgroundColor: "#9CAFEE",
+                        color: "#0F0E0E",
+                      }}
+                      onClick={() =>
+                        selectedOrder({
+                          id: row.id,
+                          date: row.date,
+                          name: row.name,
+                          address: row.address,
+                          contact: row.contact,
+                          email: row.email,
+                          Name: row.Name,
+                          Address: row.Address,
+                          Contact: row.Contact,
+                          item: row.item,
+                          quantity: row.quantity,
+                          weight: row.weight,
+                        })
+                      }
+                    >
+                      Update
+                    </Button>
+                    <Button
+                      sx={{
+                        margin: "0px 10px",
+                        backgroundColor: "#EE9461",
+                        color: "#0F0E0E",
+                      }}
+                      onClick={() => deleteOrder({ id: row.id })}
+                    >
+                      Delete
+                    </Button>
+                  </TableCell>
                 </TableRow>
-            </TableHead>
-            <TableBody>
-                {
-                filteredRows.length > 0 ? filteredRows.map(row =>(
-                    <TableRow key={row.id} sx={{'&:last-child td,&:last-child th':{border:0},
-                    backgroundColor:'#BEE8D6',
-                    }} >
-                          <TableCell component='th' scope="row">{row.id}</TableCell>
-                          <TableCell component='th' scope="row">{row.date}</TableCell>
-                          <TableCell component='th' scope="row">{row.name}</TableCell>
-                          <TableCell component='th' scope="row">{row.address}</TableCell>
-                          <TableCell component='th' scope="row">{row.contact}</TableCell>
-                          <TableCell component='th' scope="row">{row.email}</TableCell>
-                          <TableCell component='th' scope="row">{row.Name}</TableCell>
-                          <TableCell component='th' scope="row">{row.Address}</TableCell>
-                          <TableCell component='th' scope="row">{row.Contact}</TableCell>
-                          <TableCell component='th' scope="row">{row.item}</TableCell>
-                          <TableCell component='th' scope="row">{row.quantity}</TableCell>
-                          <TableCell component='th' scope="row">{row.weight}</TableCell>
-
-                          <TableCell>
-                            <Button
-                                sx={{margin:'0px 10px', backgroundColor: '#9CAFEE',color: '#0F0E0E' }}
-                                onClick={() => selectedOrder({id:row.id ,date:row.date, name:row.name,address:row.address,contact:row.contact,email:row.email,Name:row.Name,Address:row.Address,Contact:row.Contact,item:row.item,quantity:row.quantity,weight:row.weight})}                            
-                            >
-                                Update
-                            </Button>
-                            <Button
-                                sx={{margin:'0px 10px',backgroundColor:'#EE9461',color: '#0F0E0E'}} 
-                                onClick={() => deleteOrder({id: row.id})}
-                             >
-                                Delete
-                            </Button>                           
-                          </TableCell>
-                    </TableRow>                 
-                )) : (
-                    <TableRow  sx={{'&:last-child td,&:last-child th':{border:0}}}>
-                        <TableCell component='th' scope="row">No Data</TableCell>
-                    </TableRow>
-                )
-             }      
-            </TableBody>
+              ))
+            ) : (
+              <TableRow
+                sx={{ "&:last-child td,&:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  No Data
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
         </Table>
-    </TableContainer>
+      </TableContainer>
+      <div className="text-center mt-4">
+        <Button variant="contained" color="primary" onClick={handleDownloadPdf}>
+          Download PDF
+        </Button>
+      </div>
     </div>
-    );
-
-}
+  );
+};
 export default OrdersTable;
