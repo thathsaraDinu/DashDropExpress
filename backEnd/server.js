8b803f2a245c876c99ed0202afae59b0814f606c
thathsaraDinu <it22306968@my.sliit.lk>
2024-07-16 22:02:51 +0530

    serverjsupdate

--- backEnd/server.js
+++ backEnd/server.js
@@ -3,61 +3,51 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-// Importing routers
+var host;
 const router = require("./UserManagement/router");
 const router1 = require("./DeliveryManagement/router");
 const router2 = require("./Starratings/router");
-const ticket = require("./HelpDesk/router");
+const ticket = require("./HelpDesk/router")
 const vehicle = require("./FleetManagement/router");
-const packageRouter = require("./PackageManagement/router");
+const package = require("./PackageManagement/router");
 const Orders = require("./OrderManagement/router");
 
-// Importing MongoDB URI from config
 const { MONGODB_URI } = require("./config");
 
-// Setting port based on environment variable or defaulting to 3001
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3001; 
 
-// Setting host based on port
 const host = port === 3001 ? "localhost" : "0.0.0.0";
 
-// Middleware
-app.use(express.json()); // Replace bodyParser with express.json() for body parsing
-app.use(cors()); // Enable CORS for all routes
-app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
 
-// Connect to MongoDB
-const connectDB = async () => {
+app.use(express.json());
+
+app.use(cors());
+const bodyParser = require("body-parser");
+app.use(bodyParser.json({ limit: "100mb" }));
+
+const uri = MONGODB_URI;
+const connect = async () => {
   try {
-    await mongoose.connect(MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true, // Add this option to avoid deprecation warnings
-    });
-    console.log("Connected to MongoDB");
+    await mongoose.connect(uri);
+    console.log("connected to MongoDB");
   } catch (error) {
-    console.error("MongoDB connection error:", error);
-    process.exit(1); // Exit process on connection error
+    console.log("MongoDB error", error);
   }
 };
 
-// Call connectDB function to establish MongoDB connection
-connectDB();
+connect();
 
-// Routes
+app.listen(port, host, (err) => {
+  if (err) throw err;
+  console.log(`Node server is listening to ${port}`);
+
+});
+
 app.use("/api", router);
 app.use("/api", router1);
 app.use("/api", router2);
 app.use("/api", ticket);
 app.use("/api", vehicle);
-app.use("/api", packageRouter);
+app.use("/api", package);
 app.use("/api", Orders);
 
-// Start server
-app.listen(port, host, (err) => {
-  if (err) {
-    console.error("Server start error:", err);
-    process.exit(1); // Exit process on server start error
-  }
-  console.log(`Node server is listening on http://${host}:${port}`);
-});
